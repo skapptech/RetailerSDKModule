e6d249444c8df94f037a2389c56c222fb1d082ac
goleyogesh <yogesh@shopkirana.com>
2025-05-02 12:01:31 +0530

    commit

--- app/src/main/java/app/retailer/krina/shop/com/mp_shopkrina_retailer/MainActivityTest.kt
+++ app/src/main/java/app/retailer/krina/shop/com/mp_shopkrina_retailer/MainActivityTest.kt
@@ -3,19 +3,17 @@ package app.retailer.krina.shop.com.mp_shopkrina_retailer
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import app.retailer.krina.shop.com.mp_shopkrina_retailer.databinding.ActivityMainBinding
+import app.retailer.krina.shop.com.mp_shopkrina_retailer.databinding.ActivityMainTestBinding
 import app.retailer.krina.shop.com.mp_shopkrina_retailer.ui.component.splash.SplashScreenActivity
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-
+class MainActivityTest : AppCompatActivity() {
+    private lateinit var binding: ActivityMainTestBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainTestBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
-
         binding.fab.setOnClickListener {
             val intent = Intent(applicationContext, SplashScreenActivity::class.java)
             startActivity(intent)
